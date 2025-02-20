4695e65dbb0963db866a8a452010aad2b820255e
Bernard R <dev.bernard.rogier@gmail.com>
2025-02-20 12:04:23 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -109,5 +109,7 @@ function addSecurityHeaders(response: NextResponse) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|manifest.webmanifest|manifest.json).*)",
+  ],
 };
